049c130761575339ac6f1b77e96a128e44c41f37
Yuki Fujihara <mail@fujiy.dev>
2023-04-06 13:22:03 +0900

    Add websocket endpoint

--- app.py
+++ app.py
@@ -1,9 +1,12 @@
 import asyncio
 import hashlib
 import json
+from collections import defaultdict
+from datetime import datetime, timezone, tzinfo
 
 import psycopg2
 from tornado.web import Application, RequestHandler
+from tornado.websocket import WebSocketHandler
 
 
 class MainHandler(RequestHandler):
@@ -57,19 +60,19 @@ class BaseHandler(RequestHandler):
                     'name': name,
                     'client': client,
                     'admin': password == system_password,
-                    'active_flight': active_flight
+                    'activeFlight': active_flight
                     }
 
     def get_flight(self, id, recursive=True):
         with self.db.cursor() as cursor:
-            sql = 'SELECT system_id, name, start_time, launch_time, end_time'\
-                ' FROM flights WHERE id = %s'
+            sql = 'SELECT system_id, name, start_time, launch_time, end_time, '\
+                'data FROM flights WHERE id = %s'
             cursor.execute(sql, [id])
             result = cursor.fetchone()
             if result is None:
                 return None
 
-            system_id, name, start_time, launch_time, end_time = result
+            system_id, name, start_time, launch_time, end_time, data = result
 
             if recursive:
                 system = self.get_system(system_id, self.get_password(),
@@ -80,9 +83,10 @@ class BaseHandler(RequestHandler):
             return {'id': id,
                     'system': system,
                     'name': name,
-                    'start_time': start_time and start_time.isoformat() + 'Z',
-                    'launch_time': launch_time and launch_time.isoformat() + 'Z',
-                    'end_time': end_time and end_time.isoformat() + 'Z',
+                    'startTime': start_time and start_time.isoformat() + 'Z',
+                    'launchTime': launch_time and launch_time.isoformat() + 'Z',
+                    'endTime': end_time and end_time.isoformat() + 'Z',
+                    'data': data
                     }
 
 
@@ -104,7 +108,7 @@ class SystemListHandler(BaseHandler):
                 systems.append({'id': id,
                                 'name': name,
                                 'client': client,
-                                'active_flight': active_flight
+                                'activeFlight': active_flight
                                 })
 
             self.write(json.dumps({'systems': systems}))
@@ -178,9 +182,9 @@ class FlightListHandler(BaseHandler):
                     {'id': id,
                      'name': name,
                      'system': system_id,
-                     'start_time': start_time and start_time.isoformat() + 'Z',
-                     'launch_time': launch_time and launch_time.isoformat() +'Z',
-                     'end_time': end_time and end_time.isoformat() + 'Z',
+                     'startTime': start_time and start_time.isoformat() + 'Z',
+                     'launchTime': launch_time and launch_time.isoformat() +'Z',
+                     'endTime': end_time and end_time.isoformat() + 'Z',
                      })
 
             self.write(json.dumps({'flights': flights}))
@@ -196,6 +200,8 @@ class FlightListHandler(BaseHandler):
             with self.db.cursor() as cursor:
                 name = self.get_argument('name')
                 activate = self.get_argument('activate', False)
+
+                print('new flight', name, activate)
 
                 sql = 'INSERT INTO flights'\
                     ' (system_id, name, start_time, launch_time)'\
@@ -219,13 +225,101 @@ class FlightListHandler(BaseHandler):
 
 
 class FlightHandler(BaseHandler):
-    def get(self, system_id, id):
+    def get(self, id):
         flight = self.get_flight(id)
 
         if flight is None:
             self.send_error(404)
         else:
             self.write(json.dumps(flight))
+
+class PacketsHandler(WebSocketHandler):
+
+    listeners = defaultdict(lambda: [])
+
+    @property
+    def db(self):
+        return self.application.db
+
+    def check_origin(self, origin):
+        return True
+
+    def open(self, flight_id):
+        self.flight_id = flight_id
+        self.source = self.get_argument('source')
+        print('open connection', flight_id, self.source)
+        start_time = self.get_argument('startTime', None)
+        self.start_time = datetime.fromisoformat(start_time) \
+            if start_time else None
+        end_time = self.get_argument('endTime', None)
+        self.end_time = datetime.fromisoformat(end_time) \
+            if end_time else None
+
+        if self.start_time:
+            PacketsHandler.listeners[self.flight_id].append(self)
+
+            with self.db.cursor() as cursor:
+                if self.end_time:
+                    sql = 'SELECT time, source, raw FROM packets '\
+                        'WHERE flight_id = %s AND time BETWEEN %s AND %s'
+                    cursor.execute(sql, [self.flight_id, self.start_time,
+                                         self.end_time])
+                else:
+                    sql = 'SELECT time, source, raw FROM packets '\
+                        'WHERE flight_id = %s AND time > %s'
+                    cursor.execute(sql, [self.flight_id, self.start_time])
+
+                result = cursor.fetchall()
+
+                for time, source, raw in result:
+                    self.send_packet(time, source, raw)
+
+
+    def on_message(self, message):
+        if not isinstance(message, bytes):
+            return
+
+        unix_time = int.from_bytes(message[:8], 'little')
+        time = datetime.fromtimestamp(unix_time / 1000.0, timezone.utc)
+        type = message[8]
+        from_ = message[9]
+        raw = message[8:]
+
+        # print('message', time, type, from_)
+
+        for listener in PacketsHandler.listeners[self.flight_id]:
+            if listener is not self:
+                listener.send_packet(time, self.source, raw)
+
+        with self.db.cursor() as cursor:
+            sql = 'INSERT INTO packets'\
+                ' (flight_id, time, source, type, from_, raw) '\
+                'VALUES (%s, %s, %s, %s, %s, %s)'
+            cursor.execute(
+                sql,
+                (self.flight_id, time, self.source, type, from_, raw))
+
+            self.db.commit()
+
+    def send_packet(self, time, source, raw):
+        time = time.replace(tzinfo=timezone.utc)
+        if not self.start_time or time < self.start_time:
+            return
+        if self.end_time and self.end_time < time:
+            return
+
+        bytes_ = bytearray(int(time.timestamp() * 1000.0).to_bytes(8, 'little'))
+        bytes_.extend(source.encode('utf-8'))
+        bytes_.extend(bytearray(16 - len(bytes_)))
+        bytes_.extend(raw)
+        try:
+            self.write_message(bytes(bytes_), True)
+        except:
+            self.on_close()
+
+    def on_close(self):
+        print('close connection', self.flight_id, self.source)
+        PacketsHandler.listeners[self.flight_id].remove(self)
 
 
 class App(Application):
@@ -235,7 +329,8 @@ class App(Application):
             (r'/api/systems/?', SystemListHandler),
             (r'/api/systems/([^/]+)/?', SystemHandler),
             (r'/api/systems/([^/]+)/flights/?', FlightListHandler),
-            (r'/api/systems/([^/]+)/flights/([^/]+)/?', FlightHandler),
+            (r'/api/flights/([^/]+)/?', FlightHandler),
+            (r'/api/flights/([^/]+)/packets/?', PacketsHandler),
         ]
         Application.__init__(self, handlers, debug=True)
 
--- initdb/initialize.sql
+++ initdb/initialize.sql
@@ -18,13 +18,15 @@ CREATE TABLE flights (
   name TEXT,
   start_time TIMESTAMP,
   launch_time TIMESTAMP,
-  end_time TIMESTAMP
+  end_time TIMESTAMP,
+  data TEXT
 );
 
 CREATE TABLE packets (
   id serial PRIMARY KEY,
   flight_id INTEGER REFERENCES flights(id),
   time TIMESTAMP,
+  source VARCHAR(8),
   type SMALLINT,
   from_ SMALLINT,
   raw BYTEA
