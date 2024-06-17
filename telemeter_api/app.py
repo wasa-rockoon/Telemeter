08e752b00710b8ed59265a91d39b627316a3275d
Tetsuka <rogue1starwars728@yahoo.co.jp>
2024-06-17 23:57:21 +0900

    added function to add data to influxdb when message received

--- telemeter_api/app.py
+++ telemeter_api/app.py
@@ -4,28 +4,34 @@ import tornado.websocket
 from api import devices
 
 class WebSocketHandler(tornado.websocket.WebSocketHandler):
-    clients = []  # Add this line
+    clients = {}
 
     def check_origin(self, origin):
         return True
     
     def open(self):
         print("WebSocket opened")
-        self.clients.append(self)  # Add this line
+        client_name = self.get_argument('client_name', 'anonymous')  # Get the client name
+        self.client_name = client_name  # Store the client name
+        if client_name in self.clients:  # If a client with the same name already exists
+            self.clients[client_name].close()  # Close the old client's connection
+        self.clients[client_name] = self  # Store the WebSocketHandler instance
 
     def on_message(self, message):
         print(message)
         self.write_message(u"You mentioned: " + message)
-        # devices.write_measurement(message)
+        devices.write_measurement(message)
+
 
     def on_close(self):
         print("WebSocket closed")
-        self.clients.remove(self)  # Add this line
+        if self in self.clients.values():  # Only remove the client if it's still in the clients dictionary
+            del self.clients[self.client_name]  # Remove the WebSocketHandler instance
 
     @classmethod
-    def send_to_clients(cls, message):  # Add this method
+    def send_to_clients(cls, message):
         print(cls.clients)
-        for client in cls.clients:
+        for client in cls.clients.values():
             client.write_message(message)
 
 class ApiHandler(tornado.web.RequestHandler):  # Add this class
