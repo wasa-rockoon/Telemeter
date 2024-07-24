4b1a163297f47a644ddf15b72ef7cce53b011928
Ryota Tetsuka <87596452+rogue1starwars@users.noreply.github.com>
2024-07-25 01:18:52 +0900

    Update esp_sample_code.ino

--- esp_sample_code.ino
+++ esp_sample_code.ino
@@ -1,91 +1,122 @@
+/*
+ * WebSocketClient.ino
+ *
+ *  Created on: 24.05.2015
+ *
+ */
+
 #include <WiFi.h>
+#include <WiFiMulti.h>
+#include <WiFiClientSecure.h>
 
-#include <WebSocketClient.h>
+#include <WebSocketsClient.h>
 
-const char* ssid     = "XXXX";
-const char* password = "XXXX"; //replace with actual id and password
-char path[] = "/ws";
-char host[] = "192.168.18.8:5000"; //replace with actual address
-  
-WebSocketClient webSocketClient;
 
-// Use WiFiClient class to create TCP connections
-WiFiClient client;
+WiFiMulti WiFiMulti;
+WebSocketsClient webSocket;
 
-void setup() {
-  Serial.begin(115200);
-  delay(10);
+void floatToBinary(const float& value, uint8_t* buffer) {
+  memcpy(buffer, &value, sizeof(float));
+}
 
-  // We start by connecting to a WiFi network
+void hexdump(const void *mem, uint32_t len, uint8_t cols = 16) {
+	const uint8_t* src = (const uint8_t*) mem;
+	Serial.printf("\n[HEXDUMP] Address: 0x%08X len: 0x%X (%d)", (ptrdiff_t)src, len, len);
+	for(uint32_t i = 0; i < len; i++) {
+		if(i % cols == 0) {
+			Serial.printf("\n[0x%08X] 0x%08X: ", (ptrdiff_t)src, i);
+		}
+		Serial.printf("%02X ", *src);
+		src++;
+	}
+	Serial.printf("\n");
+}
 
-  Serial.println();
-  Serial.println();
-  Serial.print("Connecting to ");
-  Serial.println(ssid);
-  
-  WiFi.begin(ssid, password);
-  
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
-    Serial.print(".");
-  }
+void webSocketEvent(WStype_t type, uint8_t * payload, size_t length) {
 
-  Serial.println("");
-  Serial.println("WiFi connected");  
-  Serial.println("IP address: ");
-  Serial.println(WiFi.localIP());
+	switch(type) {
+		case WStype_DISCONNECTED:
+			Serial.printf("[WSc] Disconnected!\n");
+			break;
+		case WStype_CONNECTED:
+			Serial.printf("[WSc] Connected to url: %s\n", payload);
 
-  delay(5000);
-  
+			// send message to server when Connected
+			break;
+		case WStype_TEXT:
+			Serial.printf("[WSc] get text: %s\n", payload);
 
-  // Connect to the websocket server
-  if (client.connect("192.168.18.8", 5000)) {
-    Serial.println("Connected");
-  } else {
-    Serial.println("Connection failed.");
-    while(1) {
-      // Hang on failure
-    }
-  }
+			// send message to server
+			// webSocket.sendTXT("message here");
+			break;
+		case WStype_BIN:
+			Serial.printf("[WSc] get binary length: %u\n", length);
+			hexdump(payload, length);
 
-  // Handshake with the server
-  webSocketClient.path = path;
-  webSocketClient.host = host;
-  if (webSocketClient.handshake(client)) {
-    Serial.println("Handshake successful");
-  } else {
-    Serial.println("Handshake failed.");
-    while(1) {
-      // Hang on failure
-    }  
-  }
+			// send data to server
+			// webSocket.sendBIN(payload, length);
+			break;
+		case WStype_ERROR:			
+		case WStype_FRAGMENT_TEXT_START:
+		case WStype_FRAGMENT_BIN_START:
+		case WStype_FRAGMENT:
+		case WStype_FRAGMENT_FIN:
+			break;
+	}
 
 }
 
+void setup() {
+	// Serial.begin(921600);
+	Serial.begin(115200);
+
+	//Serial.setDebugOutput(true);
+
+	Serial.println();
+	Serial.println();
+	Serial.println();
+
+	for(uint8_t t = 4; t > 0; t--) {
+		Serial.printf("[SETUP] BOOT WAIT %d...\n", t);
+		delay(1000);
+	}
+
+	WiFiMulti.addAP("ssid", "pass");
+
+	WiFi.disconnect();
+	while(WiFiMulti.run() != WL_CONNECTED) {
+		delay(100);
+	}
+
+	// server address, port and URL
+	webSocket.begin("54.248.18.111", 80, "/ws");
+
+	// event handler
+	webSocket.onEvent(webSocketEvent);
+//
+//	// use HTTP Basic Authorization this is optional remove if not needed
+//	webSocket.setAuthorization("user", "Password");
+
+	// try ever 5000 again if connection has failed
+	webSocket.setReconnectInterval(5000);
+
+}
 
 void loop() {
-  String data;
+    static unsigned long lastSendTime = 0;
+    unsigned long currentTime = millis();
+    
+    // Check if 5 seconds have passed
+    if (currentTime - lastSendTime > 5000) {
+      float myFloat = 23.1;
+      uint8_t binaryRepresentation[sizeof(float)];
 
-  if (client.connected()) {
-    
-    webSocketClient.getData(data);
-    if (data.length() > 0) {
-      Serial.print("Received data: ");
-      Serial.println(data);
+      floatToBinary(myFloat, binaryRepresentation);
+      Serial.println(sizeof(binaryRepresentation));
+      
+        webSocket.sendBIN(binaryRepresentation, sizeof(binaryRepresentation));
+        lastSendTime = currentTime;
     }
-   
-    data = "hello from esp32";
-    
-    webSocketClient.sendData(data);
-    
-  } else {
-    Serial.println("Client disconnected.");
-    while (1) {
-      // Hang on disconnect.
-    }
-  }
-  
-  // wait to fully let the client disconnect
-  delay(3000);
-  
+
+    webSocket.loop();
 }
