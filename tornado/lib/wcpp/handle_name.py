cfcd6a6adc2336080079134545df79ef21f75216
Tetsuka <rogue1starwars728@yahoo.co.jp>
2024-07-31 12:35:03 +0900

    modified json

--- tornado/lib/wcpp/handle_name.py
+++ tornado/lib/wcpp/handle_name.py
@@ -5,7 +5,14 @@ script_dir = os.path.dirname(os.path.abspath(__file__))
 json_file_path = os.path.join(script_dir, "id_name_mapping.json")
 
 with open(json_file_path, "r") as f:
-    id_name_mapping = json.load(f)
+
+    id_name_mapping_raw = json.load(f)
+
+id_name_mapping = {}
+for key, value in id_name_mapping_raw.items():
+    id_name_mapping[key.lower()] = {}
+    for key, value in value.items():
+        id_name_mapping[key.lower()][key] = value.lower()
 
 
 def handle_name(name: str, name_type: str):
